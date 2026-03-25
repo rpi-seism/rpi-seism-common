28214ca510bce5081e54abec5cfb9779235d25b2
ch3p4ll3 <slinucs@gmail.com>
2026-03-25 10:23:00 +0100

    add to_json property

--- src/rpi_seism_common/websocket_message/__init__.py
+++ src/rpi_seism_common/websocket_message/__init__.py
@@ -8,3 +8,7 @@ class WebsocketMessage(BaseModel):
     """Base class for all websocket messages"""
     type: WebsocketMessageTypeEnum
     timestamp: datetime = Field(default_factory=lambda: datetime.now(UTC))
+
+    @property
+    def to_json(self):
+        return self.model_dump_json()
